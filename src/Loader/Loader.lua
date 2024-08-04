f581c1c7aff3acfbd707e29b4a560314ba2e6627
Matija <115701747+Maatijaa@users.noreply.github.com>
2024-08-04 03:17:59 +0200

    Updated Loader Script, added debug messages you can see them by using F9 and opening Dev Console..

--- src/Loader/Loader.lua
+++ src/Loader/Loader.lua
@@ -6,9 +6,26 @@
 --
 -- This Script simply loads the ServerManager module. And ensures that its goona load without problems.
 
+-- A bit advenced Loading System, with debug messages.. Press F9 to see them btw.
+-- You can see latest change logs on our GitHub page. https://github.com/Maatijaa-Development/ServerManager
+
+print("888888   8b   d88")
+print("88    8  88b  d88")
+print("  88     88YbdP88")
+print("    88   88 YY 88")
+print("8    88  88 YY 88")
+print(" 8888    88 YY 88")
+
+print("Loader System starting..")
+
+print("Loading ServerManager...")
 local serverManagerPath = game:GetService("ServerScriptService"):WaitForChild("SM_Module"):WaitForChild("ServerManager")
+print("ServerManager Loaded Succsesfully!")
+
+print("ServerManager Module has been loaded!")
+
 
 local ServerManager = require(serverManagerPath)
 
--- Ths just debug message if there error occurs.
-print("All modules has been succsesfully loaded!")
+print("ServerManager Module has been loaded!")
+
